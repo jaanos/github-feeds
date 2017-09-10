cd6d35ba17ac974c21c9d5def98e5bc2fdc7d425
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2017-09-11 00:31:48 +0200

    Add more events

--- bundle.js
+++ bundle.js
@@ -21,6 +21,10 @@ $.extend({
   }
 });
 
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
 var makeTime = function(result) {
   var time = document.createElement("div");
   time.className = "time";
@@ -123,8 +127,47 @@ $(function() {
       title.className = "title";
       var actor = makeLink(result, "https://github.com/" + result.actor.login, result.actor.display_login, "actor");
       var action = result.type;
-      if (["PushEvent", "IssuesEvent", "PullRequestEvent",
-           "CommitCommentEvent", "IssueCommentEvent"].indexOf(result.type) != -1) {
+      if (["CreateEvent", "DeleteEvent", "ForkEvent", "MemberEvent",
+           "WatchEvent"].indexOf(result.type) != -1) {
+        details.className = "simple";
+        if (result.type == "CreateEvent") {
+          item.className += "create";
+          if (result.payload.ref_type == "branch") {
+            details.innerHTML = "<svg aria-label=\"Create\" class=\"octicon octicon-git-branch dashboard-event-icon\" height=\"16\" role=\"img\" version=\"1.1\" viewBox=\"0 0 10 16\" width=\"10\"><path fill-rule=\"evenodd\" d=\"M10 5c0-1.11-.89-2-2-2a1.993 1.993 0 0 0-1 3.72v.3c-.02.52-.23.98-.63 1.38-.4.4-.86.61-1.38.63-.83.02-1.48.16-2 .45V4.72a1.993 1.993 0 0 0-1-3.72C.88 1 0 1.89 0 3a2 2 0 0 0 1 1.72v6.56c-.59.35-1 .99-1 1.72 0 1.11.89 2 2 2 1.11 0 2-.89 2-2 0-.53-.2-1-.53-1.36.09-.06.48-.41.59-.47.25-.11.56-.17.94-.17 1.05-.05 1.95-.45 2.75-1.25S8.95 7.77 9 6.73h-.02C9.59 6.37 10 5.73 10 5zM2 1.8c.66 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2C1.35 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2zm0 12.41c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm6-8c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z\"/></svg>";
+            branch = makeLink(result, "https://github.com/" + result.repo.name + "/tree/" + result.payload.ref, result.payload.ref, "branch", result.payload.ref, "css-truncate css-truncate-target branch-name") + " at ";
+          } else {
+            details.innerHTML = "<svg aria-label=\"Create\" class=\"octicon octicon-repo dashboard-event-icon\" height=\"16\" role=\"img\" version=\"1.1\" viewBox=\"0 0 12 16\" width=\"12\"><path fill-rule=\"evenodd\" d=\"M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z\"/></svg>";
+            branch = "";
+          }
+          action = "created " + result.payload.ref_type + " " + branch + makeLink(result, "https://github.com/" + result.repo.name, result.repo.name, "repo");
+        } else if (result.type == "DeleteEvent") {
+          item.className += "delete";
+          if (result.payload.ref_type == "branch") {
+            details.innerHTML = "<svg aria-label=\"Delete\" class=\"octicon octicon-git-branch dashboard-event-icon\" height=\"16\" role=\"img\" version=\"1.1\" viewBox=\"0 0 10 16\" width=\"10\"><path fill-rule=\"evenodd\" d=\"M10 5c0-1.11-.89-2-2-2a1.993 1.993 0 0 0-1 3.72v.3c-.02.52-.23.98-.63 1.38-.4.4-.86.61-1.38.63-.83.02-1.48.16-2 .45V4.72a1.993 1.993 0 0 0-1-3.72C.88 1 0 1.89 0 3a2 2 0 0 0 1 1.72v6.56c-.59.35-1 .99-1 1.72 0 1.11.89 2 2 2 1.11 0 2-.89 2-2 0-.53-.2-1-.53-1.36.09-.06.48-.41.59-.47.25-.11.56-.17.94-.17 1.05-.05 1.95-.45 2.75-1.25S8.95 7.77 9 6.73h-.02C9.59 6.37 10 5.73 10 5zM2 1.8c.66 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2C1.35 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2zm0 12.41c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm6-8c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z\"/></svg>";
+            branch = "<span class=\"branch-name\">" + result.payload.ref + "</span> at ";
+          } else {
+            details.innerHTML = "<svg aria-label=\"Delete\" class=\"octicon octicon-repo dashboard-event-icon\" height=\"16\" role=\"img\" version=\"1.1\" viewBox=\"0 0 12 16\" width=\"12\"><path fill-rule=\"evenodd\" d=\"M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z\"/></svg>";
+            branch = "";
+          }
+          action = "deleted " + result.payload.ref_type + " " + branch + makeLink(result, "https://github.com/" + result.repo.name, result.repo.name, "repo");
+        } else if (result.type == "ForkEvent") {
+          item.className += "fork";
+          details.innerHTML = "<svg aria-label=\"Fork\" class=\"octicon octicon-repo-forked dashboard-event-icon\" height=\"16\" role=\"img\" version=\"1.1\" viewBox=\"0 0 10 16\" width=\"10\"><path fill-rule=\"evenodd\" d=\"M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z\"/></svg>";
+          action = "forked " + makeLink(result, "https://github.com/" + result.repo.name, result.repo.name, "repo") + " to " + makeLink(result, result.payload.forkee.html_url, result.payload.forkee.full_name, "parent", result.payload.forkee.full_name);
+        } else if (result.type == "MemberEvent") {
+          item.className += "member";
+          details.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" class=\"octicon octicon-person dashboard-event-icon\" height=\"16\" role=\"img\" version=\"1.1\" viewBox=\"0 0 12 16\" width=\"12\"><path fill-rule=\"evenodd\" d=\"M12 14.002a.998.998 0 0 1-.998.998H1.001A1 1 0 0 1 0 13.999V13c0-2.633 4-4 4-4s.229-.409 0-1c-.841-.62-.944-1.59-1-4 .173-2.413 1.867-3 3-3s2.827.586 3 3c-.056 2.41-.159 3.38-1 4-.229.59 0 1 0 1s4 1.367 4 4v1.002z\"/></svg>";
+          action = "added " + makeLink(result, result.payload.member.html_url, result.payload.member.login, "member") + " to " + makeLink(result, "https://github.com/" + result.repo.name, result.repo.name, "repo");
+        } else if (result.type == "WatchEvent") {
+          item.className += "watch";
+          details.innerHTML = "<svg aria-label=\"Watch\" class=\"octicon octicon-star dashboard-event-icon\" height=\"16\" role=\"img\" version=\"1.1\" viewBox=\"0 0 14 16\" width=\"14\"><path fill-rule=\"evenodd\" d=\"M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z\"/></svg>";
+          action = "starred " + makeLink(result, "https://github.com/" + result.repo.name, result.repo.name, "repo");
+        }
+        item.className += " simple";
+        details.appendChild(title);
+        details.append("\n");
+        details.appendChild(makeTime(result));
+      } else {
         details.className = "details";
         details.innerHTML = makeAvatar(result);
         if (result.type == "PushEvent") {
@@ -134,11 +177,10 @@ $(function() {
           item.className += "push";
           body.innerHTML = "<svg aria-label=\"Push\" class=\"octicon octicon-git-commit dashboard-event-icon\" height=\"32\" role=\"img\" version=\"1.1\" viewBox=\"0 0 14 16\" width=\"28\"><path fill-rule=\"evenodd\" d=\"M10.86 7c-.45-1.72-2-3-3.86-3-1.86 0-3.41 1.28-3.86 3H0v2h3.14c.45 1.72 2 3 3.86 3 1.86 0 3.41-1.28 3.86-3H14V7h-3.14zM7 10.2c-1.22 0-2.2-.98-2.2-2.2 0-1.22.98-2.2 2.2-2.2 1.22 0 2.2.98 2.2 2.2 0 1.22-.98 2.2-2.2 2.2z\"/></svg>";
           action = "pushed to " + makeLink(result, "https://github.com/" + result.repo.name + "/tree/" + branch, branch, "branch") + " at " + makeLink(result, "https://github.com/" + result.repo.name, result.repo.name, "repo")
-          var commits = document.createElement("div");
           if (result.payload.commits.length == 0) {
-            commits.className = "message";
-            commits.innerHTML = "<blockquote>" + makeLink(result, "https://github.com/" + result.repo.name + "/tree/" + branch, branch, "branch") + " is now at " + makeLink(result, "https://github.com/" + result.repo.name + "/commit/" + result.payload.head, result.payload.head.substring(0, 7), "commit") + "</blockquote>";
+            details.innerHTML += "<strong>" + branch + "</strong> is now <code>" + makeLink(result, "https://github.com/" + result.repo.name + "/commit/" + result.payload.head, result.payload.head.substring(0, 7), "commit") + "</code>";
           } else {
+            var commits = document.createElement("div");
             commits.className = "commits";
             var ul = document.createElement("ul");
             commit_reqs = [];
@@ -181,8 +223,8 @@ $(function() {
               })
             })();
             commits.appendChild(ul);
+            details.appendChild(commits);
           }
-          details.appendChild(commits);
         } else if (result.type == "IssuesEvent") {
           item.className += "issues_" + result.payload.action;
           if (result.payload.action == "closed") {
@@ -218,34 +260,32 @@ $(function() {
           }
           action = "commented on " + type + " " + makeLink(result, result.payload.comment.html_url, result.repo.name + "#" + result.payload.issue.number, "issue-comment", result.payload.issue.title);
           details.innerHTML += "\n<div class=\"message markdown-body\"><blockquote>\n" + result.payload.comment.body + "\n</blockquote>\n</div>";
+        } else if (result.type == "PullRequestReviewCommentEvent") {
+          item.className += "pull_request_review_comment";
+          body.innerHTML = "<svg aria-label=\"Review pull request comment\" class=\"octicon octicon-comment-discussion dashboard-event-icon\" height=\"32\" role=\"img\" version=\"1.1\" viewBox=\"0 0 16 16\" width=\"32\"><path fill-rule=\"evenodd\" d=\"M15 1H6c-.55 0-1 .45-1 1v2H1c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h1v3l3-3h4c.55 0 1-.45 1-1V9h1l3 3V9h1c.55 0 1-.45 1-1V2c0-.55-.45-1-1-1zM9 11H4.5L3 12.5V11H1V5h4v3c0 .55.45 1 1 1h3v2zm6-3h-2v1.5L11.5 8H6V2h9v6z\"/></svg>";
+          action = "commented on pull request " + makeLink(result, result.payload.comment.html_url, result.repo.name + "#" + result.payload.pull_request.number, "pull-request-review-comment");
+          details.innerHTML += "\n<div class=\"message markdown-body\"><blockquote>\n" + result.payload.comment.body + "\n</blockquote>\n</div>";
+        } else if (result.type == "GollumEvent") {
+          item.className += "gollum";
+          body.innerHTML = "<svg aria-label=\"Gollum\" class=\"octicon octicon-book dashboard-event-icon\" height=\"32\" role=\"img\" version=\"1.1\" viewBox=\"0 0 16 16\" width=\"32\"><path fill-rule=\"evenodd\" d=\"M3 5h4v1H3V5zm0 3h4V7H3v1zm0 2h4V9H3v1zm11-5h-4v1h4V5zm0 2h-4v1h4V7zm0 2h-4v1h4V9zm2-6v9c0 .55-.45 1-1 1H9.5l-1 1-1-1H2c-.55 0-1-.45-1-1V3c0-.55.45-1 1-1h5.5l1 1 1-1H15c.55 0 1 .45 1 1zm-8 .5L7.5 3H2v9h6V3.5zm7-.5H9.5l-.5.5V12h6V3z\"/></svg>";
+          action = "edited the " + makeLink(result, "https://github.com/" + result.repo.name, result.repo.name, "repo") + " wiki";
+          var commits = document.createElement("div");
+          commits.className = "commits";
+          var ul = document.createElement("ul");
+          for (index in result.payload.pages) {
+            var page = result.payload.pages[index];
+            var li = document.createElement("li");
+            li.innerHTML = page.action.capitalize() + " " + makeLink(result, page.html_url, page.title, "wiki-page") + "."
+            if (page.action == "edited") {
+              li.innerHTML += " " + makeLink(result, page.html_url + "/_compare/" + page.sha.substring(0, 7) + "^..." + page.sha.substring(0, 7), "View the diff »", "wiki-compare");
+            }
+            ul.appendChild(li);
+          }
+          commits.appendChild(ul);
+          details.appendChild(commits);
         }
         body.appendChild(makeTime(result));
         body.appendChild(title);
-      } else {
-        details.className = "simple";
-        if (result.type == "CreateEvent") {
-          item.className += "create";
-          if (result.payload.ref_type == "branch") {
-            details.innerHTML = "<svg aria-label=\"Create\" class=\"octicon octicon-git-branch dashboard-event-icon\" height=\"16\" role=\"img\" version=\"1.1\" viewBox=\"0 0 10 16\" width=\"10\"><path fill-rule=\"evenodd\" d=\"M10 5c0-1.11-.89-2-2-2a1.993 1.993 0 0 0-1 3.72v.3c-.02.52-.23.98-.63 1.38-.4.4-.86.61-1.38.63-.83.02-1.48.16-2 .45V4.72a1.993 1.993 0 0 0-1-3.72C.88 1 0 1.89 0 3a2 2 0 0 0 1 1.72v6.56c-.59.35-1 .99-1 1.72 0 1.11.89 2 2 2 1.11 0 2-.89 2-2 0-.53-.2-1-.53-1.36.09-.06.48-.41.59-.47.25-.11.56-.17.94-.17 1.05-.05 1.95-.45 2.75-1.25S8.95 7.77 9 6.73h-.02C9.59 6.37 10 5.73 10 5zM2 1.8c.66 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2C1.35 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2zm0 12.41c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm6-8c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z\"/></svg>"
-            branch = makeLink(result, "https://github.com/" + result.repo.name + "/tree/" + result.payload.ref, result.payload.ref, "branch", result.payload.ref, "css-truncate css-truncate-target branch-name") + " at ";
-          } else {
-            details.innerHTML = "<svg aria-label=\"Create\" class=\"octicon octicon-repo dashboard-event-icon\" height=\"16\" role=\"img\" version=\"1.1\" viewBox=\"0 0 12 16\" width=\"12\"><path fill-rule=\"evenodd\" d=\"M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z\"/></svg>";
-            branch = "";
-          }
-          action = "created " + result.payload.ref_type + " " + branch + makeLink(result, "https://github.com/" + result.repo.name, result.repo.name, "repo");
-        } else if (result.type == "DeleteEvent") {
-          item.className += "delete";
-          details.innerHTML = "<svg aria-label=\"Delete\" class=\"octicon octicon-git-branch dashboard-event-icon\" height=\"16\" role=\"img\" version=\"1.1\" viewBox=\"0 0 10 16\" width=\"10\"><path fill-rule=\"evenodd\" d=\"M10 5c0-1.11-.89-2-2-2a1.993 1.993 0 0 0-1 3.72v.3c-.02.52-.23.98-.63 1.38-.4.4-.86.61-1.38.63-.83.02-1.48.16-2 .45V4.72a1.993 1.993 0 0 0-1-3.72C.88 1 0 1.89 0 3a2 2 0 0 0 1 1.72v6.56c-.59.35-1 .99-1 1.72 0 1.11.89 2 2 2 1.11 0 2-.89 2-2 0-.53-.2-1-.53-1.36.09-.06.48-.41.59-.47.25-.11.56-.17.94-.17 1.05-.05 1.95-.45 2.75-1.25S8.95 7.77 9 6.73h-.02C9.59 6.37 10 5.73 10 5zM2 1.8c.66 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2C1.35 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2zm0 12.41c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm6-8c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z\"/></svg>"
-          action = "deleted " + result.payload.ref_type + " <span class=\"branch-name\">" + result.payload.ref + "</span> at " + makeLink(result, "https://github.com/" + result.repo.name, result.repo.name, "repo")
-        } else if (result.type == "ForkEvent") {
-          item.className += "fork";
-          details.innerHTML = "<svg aria-label=\"Fork\" class=\"octicon octicon-repo-forked dashboard-event-icon\" height=\"16\" role=\"img\" version=\"1.1\" viewBox=\"0 0 10 16\" width=\"10\"><path fill-rule=\"evenodd\" d=\"M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z\"/></svg>"
-          action = "forked " + makeLink(result, "https://github.com/" + result.repo.name, result.repo.name, "repo") + " to " + makeLink(result, result.payload.forkee.html_url, result.payload.forkee.full_name, "fork")
-        }
-        item.className += " simple";
-        details.appendChild(title);
-        details.append("\n");
-        details.appendChild(makeTime(result));
       }
       title.innerHTML = actor + " " + action;
       body.appendChild(details);
